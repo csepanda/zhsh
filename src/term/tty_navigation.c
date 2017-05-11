5a2552d6e59132521983490dc6ffb45cfef842c1
Andrey Bova <andrewbova@yandex.ru>
2017-05-11 19:01:34 -0400

    Add tty navigation module
    Add functions, types and objects to manipulate tty

---
+++ src/term/tty_navigation.c
@@ -0,0 +1,90 @@
+#include "tty_navigation.h"
+
+static void tty_navigation_object_ini_position(void);
+
+static tty_navigation_position_t* cp = &tty_navigation.current_position;
+static tty_navigation_position_t* ip = &tty_navigation.user_input_position;
+
+void tty_navigation_object_ini(void) {
+    tty_navigation_object_ini_position();
+}
+
+void tty_navigation_move(tty_navigation_direction_t direction) {
+    switch (direction) {
+    case TTY_MOVE_UP:
+        write(1, TTY_MOVE_UP_STR,    4);
+        if (cp->y > 1) cp->y--;
+        break;
+    case TTY_MOVE_DOWN:
+        write(1, TTY_MOVE_DOWN_STR,  4);
+        cp->y++;
+        break;
+    case TTY_MOVE_RIGHT:
+        write(1, TTY_MOVE_RIGHT_STR, 4);
+        cp->x++;
+        break;
+    case TTY_MOVE_LEFT:
+        write(1, TTY_MOVE_LEFT_STR,  4);
+        if (cp->x > 1) cp->x--;
+        break;
+    }
+}
+
+void tty_navigation_move_to(tty_navigation_position_t position) {
+    int i = 2;
+    char buffer[32];
+    buffer[0] = 27;
+    buffer[1] = '[';
+    i += int_to_string(position.y, buffer + i);
+    buffer[i++] = ';';
+    i += int_to_string(position.x, buffer + i);
+    buffer[i++] = 'H';
+    buffer[i] = '\0';
+    write(STDOUT, buffer, i);
+    cp->y = position.y;
+    cp->x = position.x;
+}
+
+void tty_navigation_print_debug_info(void) {
+    puts("TTY NAVIGATION OBJECT {");
+    puts("\tcurrent position {");
+    printf("\t\tx: %i\n\t\ty: %i\n\t}\n", cp->x, cp->y);
+    puts("\tuser's input position {");
+    printf("\t\tx: %i\n\t\ty: %i\n\t}\n", ip->x, ip->y);
+    puts("}");
+}
+
+
+/* initialize 'tty_navigation_position's that refers the cursor position tty */
+static void tty_navigation_object_ini_position(void) {
+    int i = 0; char buffer[32];
+
+    /* 
+     * CSI 6n :: device status report
+     * return to stdin the cursor position in format ESC[n;mR
+     *      n :: row
+     *      m :: column
+     */
+    write(1, "[6n", 4); 
+
+    read(0, buffer, 2);
+    if (buffer[0] != '' || buffer[1] != '[') {
+        _panic(PANIC_TTY_NAVIGATION_INI_POSITION_READ_ERROR, 
+               PANIC_ZHYSH_INITIALIZATION_ERROR_CODE);
+    }
+    
+    while (read(0, (buffer + i++), 1) == 1) {
+        if (buffer[i - 1] == ';') {
+            buffer[i - 1] = '\0';
+            cp->y = atoi(buffer);
+            ip->y = cp->y;
+            i = 0;
+        } else if (buffer[i - 1] == 'R') {
+            buffer[i - 1] = '\0';
+            cp->x = atoi(buffer);
+            ip->x = cp->x;
+            break;
+        }
+    }
+
+}
---
+++ src/term/tty_navigation.h
@@ -0,0 +1,45 @@
+#ifndef _ZHYSH_TTY_NAVIGATION_H
+#define _ZHYSH_TTY_NAVIGATION_H
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "../io/io.h"
+#include "../error.h"
+
+#ifndef I_REWROTE
+    #include <stdio.h>
+#endif
+
+#define TTY_MOVE_UP_STR     "[1A"
+#define TTY_MOVE_DOWN_STR   "[1B"
+#define TTY_MOVE_RIGHT_STR  "[1C"
+#define TTY_MOVE_LEFT_STR   "[1D"
+
+typedef enum tty_navigation_direction_t {
+    TTY_MOVE_UP,
+    TTY_MOVE_DOWN,
+    TTY_MOVE_RIGHT,
+    TTY_MOVE_LEFT
+} tty_navigation_direction_t;
+
+typedef struct _tty_navigation_position {
+    uint32_t x;
+    uint32_t y;
+} tty_navigation_position_t;
+
+typedef struct _tty_navigation {
+    tty_navigation_position_t current_position;
+    tty_navigation_position_t user_input_position;
+} tty_navigation_t;
+
+tty_navigation_t tty_navigation;
+
+void tty_navigation_object_ini(void);
+void tty_navigation_position_reset(void);
+void tty_navigation_move(tty_navigation_direction_t direction);
+void tty_navigation_move_to(tty_navigation_position_t position);
+void tty_navigation_print_debug_info(void);
+
+#endif
