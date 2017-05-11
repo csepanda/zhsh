f998e9c2dd17e550401329cbd45072665f4ebd85
Andrey Bova <andrewbova@yandex.ru>
2017-05-11 18:50:42 -0400

    Add initiial file of I/O module
    add defined names for default stdin, stdout, stderr fds

---
+++ src/io/io.h
@@ -0,0 +1,14 @@
+#ifndef _ZHYSH_IO_H_
+#define _ZHYSH_IO_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "../error.h"
+
+#define STDIN  0
+#define STDOUT 1
+#define STDERR 2
+
+#endif
